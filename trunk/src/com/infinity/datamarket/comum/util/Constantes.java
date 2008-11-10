01cb9dd5aafcc1251c604b2103a3ef2526f8c4ee
jonas.filho26 <jonas.filho26@6e30805c-f344-0410-b4ab-c7f1f8d58467>
2008-11-10 18:18:23 +0000

    cancelamento de entrada de produtos, totalizador de quantidade, calculo do valor total liquido subtraindo o desconto.

--- trunk/src/com/infinity/datamarket/comum/util/Constantes.java
+++ trunk/src/com/infinity/datamarket/comum/util/Constantes.java
@@ -4,6 +4,7 @@ public class Constantes {
 	public static final int NUMERO_NAO_INFORMADO = -1;
 	public static final String STATUS_ATIVO = "A";
 	public static final String STATUS_INATIVO = "I";
+	public static final String STATUS_CANCELADO = "C";
 	public static final String SIM = "S";
 	public static final String NAO = "N";
 	public static final String FORMA_CHEQUE_PRE = "3";
