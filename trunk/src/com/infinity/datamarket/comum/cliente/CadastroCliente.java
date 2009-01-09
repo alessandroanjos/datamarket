fe639da7c5e8fad2f261f8bb616939c16db36cf7
dmsvrent01@gmail.com <dmsvrent01@gmail.com@6e30805c-f344-0410-b4ab-c7f1f8d58467>
2009-01-09 02:16:39 +0000

    alterar as informacoes do cliente_transacao quando alterar um cliente que esteja em cliente_Transacao tambem.

--- trunk/src/com/infinity/datamarket/comum/cliente/CadastroCliente.java
+++ trunk/src/com/infinity/datamarket/comum/cliente/CadastroCliente.java
@@ -1,8 +1,13 @@
 package com.infinity.datamarket.comum.cliente;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
 
 import com.infinity.datamarket.comum.repositorymanager.IPropertyFilter;
+import com.infinity.datamarket.comum.repositorymanager.PropertyFilter;
+import com.infinity.datamarket.comum.transacao.ClienteTransacao;
+import com.infinity.datamarket.comum.transacao.Transacao;
 import com.infinity.datamarket.comum.util.AppException;
 import com.infinity.datamarket.comum.util.Cadastro;
 
@@ -39,6 +44,38 @@ public class CadastroCliente extends Cadastro {
 	
 	public void alterar(Cliente cliente) throws AppException{
 		getRepositorio().update(cliente);
+		IPropertyFilter filter = new PropertyFilter();
+		filter.setTheClass(ClienteTransacao.class);
+		filter.addProperty("cpfCnpj", cliente.getCpfCnpj());
+		Collection colecaoClienteTransacao = consultar(filter);
+		if(colecaoClienteTransacao != null && colecaoClienteTransacao.size() > 0){
+			Iterator itClienteTransacao = colecaoClienteTransacao.iterator();
+			while(itClienteTransacao.hasNext()){
+				ClienteTransacao cliTrans = (ClienteTransacao) itClienteTransacao.next();
+				if(cliente.getTipoPessoa().equals(cliTrans.getTipoPessoa()) &&
+						cliente.getCpfCnpj().equals(cliTrans.getCpfCnpj())){
+					if(cliente.getTipoPessoa().equals(Cliente.PESSOA_FISICA)){
+						cliTrans.setNomeCliente(cliente.getNomeCliente());	
+					}else{
+						cliTrans.setNomeCliente(cliente.getRazaoSocial());
+						cliTrans.setInscricaoEstadual(cliente.getInscricaoEstadual());
+						cliTrans.setInscricaoMunicipal(cliente.getInscricaoMunicipal());
+					}
+					cliTrans.setLogradouro(cliente.getLogradouro());
+					cliTrans.setNumero(cliente.getNumero());
+					cliTrans.setComplemento(cliente.getComplemento());
+					cliTrans.setBairro(cliente.getBairro());
+					cliTrans.setCep(cliente.getCep());
+					cliTrans.setCidade(cliente.getCidade());
+					cliTrans.setEstado(cliente.getEstado());
+					cliTrans.setFone(cliente.getFoneContato());	
+					cliTrans.setCelular(cliente.getFoneCelular());
+			    	cliTrans.setPessoaContato(cliente.getPessoaContato());
+			    	cliTrans.setReferenciaBancaria(cliente.getReferenciaComercial());
+			    	getRepositorio().update(cliTrans);
+				}
+			}
+		}
 	}
 	
 	public void excluir(Cliente cliente) throws AppException{
