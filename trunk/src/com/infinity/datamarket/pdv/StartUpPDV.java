608239741c1163033ddaa78152517cc311749e73
dmsvrent01@gmail.com <dmsvrent01@gmail.com@6e30805c-f344-0410-b4ab-c7f1f8d58467>
2009-10-16 00:53:10 +0000

    colocar para o pdv gravar traces e fazer backup

--- trunk/src/com/infinity/datamarket/pdv/StartUpPDV.java
+++ trunk/src/com/infinity/datamarket/pdv/StartUpPDV.java
@@ -1,30 +1,19 @@
 package com.infinity.datamarket.pdv;
 
-import java.io.File;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Vector;
 
 import com.infinity.datamarket.comum.repositorymanager.RepositoryManagerHibernateUtil;
 import com.infinity.datamarket.comum.util.ServiceLocator;
-import com.infinity.datamarket.comum.util.Util;
 import com.infinity.datamarket.pdv.gerenciadorperifericos.GerenciadorPerifericos;
 import com.infinity.datamarket.pdv.gui.telas.ConstantesTela;
 import com.infinity.datamarket.pdv.gui.telas.TelaMenssagem;
-import com.infinity.datamarket.pdv.maquinaestados.ConcentradorMaquina;
-import com.infinity.datamarket.pdv.maquinaestados.ControladorMaquinaEstado;
 import com.infinity.datamarket.pdv.maquinaestados.Estado;
-import com.infinity.datamarket.pdv.maquinaestados.LeitorMaquinaEstadoXML;
 import com.infinity.datamarket.pdv.maquinaestados.Maquina;
 import com.infinity.datamarket.pdv.transacao.ThreadEnviaTransacao;
 
 
 public class StartUpPDV {
 	public static void main(String[] a) throws Exception{
-		
-		backupTraces();
-		
 //		TelaInicial ti = new TelaInicial();
 //
 //		Toolkit toolkit = Toolkit.getDefaultToolkit();
@@ -66,59 +55,5 @@ public class StartUpPDV {
 //		ti.setVisible(false);
 	}
 	
-	public static void backupTraces(){
-		Vector vetorArquivos = new Vector();
-		
-		String nomeArquivoSO = "LogPDVSO.txt";
-		String nomeArquivoRT = "LogPDVRT.txt";
-		
-		if(new File("." + File.separator + nomeArquivoSO).exists()){
-			vetorArquivos.add(nomeArquivoSO);
-		}
-		
-		if(new File("." + File.separator + nomeArquivoRT).exists()){
-			vetorArquivos.add(nomeArquivoRT);
-		}
-		
-		String arquivoBackup = "";
-		Calendar c = new GregorianCalendar();
-
-		int dia  = c.get(Calendar.DAY_OF_MONTH);
-		int mes  = c.get(Calendar.MONTH)+1;
-		int ano  = c.get(Calendar.YEAR);
-		int hora = c.get(Calendar.HOUR_OF_DAY);
-		int min  = c.get(Calendar.MINUTE);
-		int seg  = c.get(Calendar.SECOND);
-
-		String diretorio = "." + File.separator + "backupTraces";
-		File f = null;
-		f = new File(diretorio);
-		if(!f.exists()){
-			f.mkdir();	
-		}
-		
-		String diretorioCompleto = 	(diretorio.endsWith(File.separator)?diretorio:diretorio + File.separator) +
-									ano + Util.completaString(""+mes, "0", 2, true) + Util.completaString(""+dia,"0", 2, true);
-
-		f = new File(diretorioCompleto);
-		if(!f.exists()){
-			f.mkdir();	
-		}
-		
-		arquivoBackup = "Trace" + "_" + ano +
-		Util.completaString(""+mes, "0", 2, true) +
-		Util.completaString(""+dia, "0", 2, true) + "_" +
-		Util.completaString(""+hora, "0", 2, true) +
-		Util.completaString(""+min, "0", 2, true) +
-		Util.completaString(""+seg, "0", 2, true);
-
-		String[] arquivosParaCompactacao = new String[vetorArquivos.size()];
-		vetorArquivos.copyInto(arquivosParaCompactacao);
-
-		Util.compactaArquivoZip(arquivosParaCompactacao,
-				(diretorioCompleto.endsWith(File.separator)?
-					diretorioCompleto+ arquivoBackup:
-					diretorioCompleto + File.separator + arquivoBackup));
-
-	}
+	
 }
